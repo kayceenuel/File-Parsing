b3f9eb0a405e482706419fe0d1a7753c8992539e
Kaycee Nuel <heykayceee@gmail.com>
2025-03-31 22:54:21 +0100

    added func to read binary data

--- parsers/binary/binary.go
+++ parsers/binary/binary.go
@@ -1,7 +1,9 @@
 package binary
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
 )
 
 // Parser interface
@@ -12,6 +14,15 @@ type PlayerRecord struct {
 
 type parser struct{}
 
+// parse reads binary data from the provided reader and returns a slice of playersRecord
 func (p *parser) Parse(r io.Reader) ([]PlayersRecord, error) {
-
+	// Read all data from the reader into a byte slice.
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read data: %v", err)
+	}
+	// ensure there's enough data for the endianess maker
+	if len(data) < 2 {
+		return nil, fmt.Errorf("insufficient data for endianness marker")
+	}
 }
